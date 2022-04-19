f645286a571809412c489927c926ffdefd126c14
Irulbotz <101655172+Zairulkhaq@users.noreply.github.com>
2022-04-19 20:12:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Bismillahirrahmanirrahim
 // thank you to ALLAH Swt
-// thank you to Nurutomo as wabot-aq
+// thank you to zairulkhaq as wabot-aq
 // thank you to ariffb as stikerinbot
 // thank you to botstylee
 // thank you to bochilgaming as games-wabot
@@ -13,9 +13,9 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.linkGC = ['https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV', 'https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
-global.owner = ['6282146218274', '6282146218274', '6282146218274'] // Masukan nomot kalian
-global.mods = ['6282146218274', '6287892711054'] // Moderator
+global.linkGC = ['https://chat.whatsapp.com/Jzd9https://chat.whatsapp.com/ifegqt8oimt0sdwr6aq28s, 'https://chat.whatsapp.com/ /https://chat.whatsapp.com/IFEgqT8oimt0sdWR6Aq28S/ ganti jadi group lu
+global.owner = ['081334690126', '081334690126', '081334690126'] // Masukan nomot kalian
+global.mods = ['081334690126', '081334690126'] // Moderator
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -55,7 +55,7 @@ const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
   var sticker_name = 'hehe'
-  var sticker_author = 'by RielBotz'
+  var sticker_author = 'by IrulBotz'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -79,7 +79,7 @@ global.nsfw = '“Katakanlah kepada orang laki-laki yang beriman: Hendaklah mere
 global.eror = '_*Error*_'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23101820&text='
-global.watermark = '©RIEL-botz' //change the watermark (beberapa harus manual)
+global.watermark = '© IRUL-botz' //change the watermark (beberapa harus manual)
 
 // Ubah saja
 global.image = 'https://telegra.ph/file/43a5bfd09e6c7e9a8055f.jpg'
